a5659739f1f7e25b9b8b5e267c3f00e71a9c51e7
Cory <ppmtscory@users.noreply.github.com>
2017-07-27 11:57:24 -0500

    Update index.js
    updating crypto

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ function encrypt(plainText, password, cb) {
     var salt = new Buffer(crypto.randomBytes(16), 'binary');
     var iv = new Buffer(crypto.randomBytes(16), 'binary');
 
-    crypto.pbkdf2(password, salt, 1000, 32, function (err, key) {
+    crypto.pbkdf2(password, salt, 1000, 32, 'sha512', function (err, key) {
         if (err) {
             logger.error('Failed to generate key.', err);
             cb(err, null);
@@ -250,8 +250,8 @@ function encrypt(plainText, password, cb) {
         var buffer = new Buffer(cipher.update(plainText, 'utf8', 'binary'), 'binary');
         buffer = Buffer.concat([buffer, new Buffer(cipher.final('binary'), 'binary')]);
 
-        var hashKey = crypto.createHash('sha1').update(key).digest('binary');
-        var hmac = new Buffer(crypto.createHmac('sha1', hashKey).update(buffer).digest('binary'), 'binary');
+        var hashKey = crypto.createHash('sha512').update(key).digest('binary');
+        var hmac = new Buffer(crypto.createHmac('sha512', hashKey).update(buffer).digest('binary'), 'binary');
 
         buffer = Buffer.concat([salt, iv, hmac, buffer]);
         cb(null, buffer.toString('base64'));
@@ -266,7 +266,7 @@ function decrypt(cipherText, password, cb) {
     var hmac = cipher.slice(32, 52);
     cipherText = cipher.slice(52);
 
-    crypto.pbkdf2(password, salt, 1000, 32, function (err, key) {
+    crypto.pbkdf2(password, salt, 1000, 32, 'sha512', function (err, key) {
         if (err) {
             logger.error('Failed to generate key.', err);
             cb(err, null);
@@ -275,8 +275,8 @@ function decrypt(cipherText, password, cb) {
         var cipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
 
         // Verify the HMAC first
-        var hashKey = crypto.createHash('sha1').update(key).digest('binary');
-        var hmacgen = new Buffer(crypto.createHmac('sha1', hashKey).update(cipherText).digest('binary'), 'binary');
+        var hashKey = crypto.createHash('sha512').update(key).digest('binary');
+        var hmacgen = new Buffer(crypto.createHmac('sha512', hashKey).update(cipherText).digest('binary'), 'binary');
         if (hmacgen.toString('base64') !== hmac.toString('base64')) {
             cb(new Error('HMAC Mismatch!'), null);
             return;
